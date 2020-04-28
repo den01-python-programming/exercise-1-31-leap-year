aeb8887d7f3c4e75947231d74ac2eeceb9864473
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 09:16:09 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,7 +1,34 @@
 import pytest
-from src.exercise import main
+import src.exercise
 
-def test_exercise(capsys):
-    main()
-    out, err = capsys.readouterr()
-    assert out == "\n", "Should read ''"
+def test_exercise():
+    input_values = ["2016","2010","1200"]
+    input_values_stored = ["2016","2010","1200"]
+    output = []
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    assert [output[0],output[1]] == ["Give a year:","The year is a leap year."]
+    assert [output[2],output[3]] == ["Give a year:","The year is not a leap year."]
+    assert [output[4],output[5]] == ["Give a year:","The year is not a leap year."]
